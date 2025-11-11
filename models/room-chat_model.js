27b9ffa4ad46c7abef9857331d2e9136d439489f
2uy2 <quytran11204@gmail.com>
2025-11-11 16:50:54 +0700

    tạo collection rooms-chat

---
+++ models/room-chat_model.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const roomChatSchema = new mongoose.Schema({
+    title: String,
+    avatar: String,
+    theme: String,
+    typeRoom: String, //friend or group
+    status: String,
+    users: [
+        {
+            user_id:String,
+            role:String,
+            
+        }
+    ],
+    deletedAt: Date // tự thêm trường dữ liệu
+}, {
+    timestamps: true // thời gian khởi tạo
+});
+const RoomChat = mongoose.model("RoomChat", roomChatSchema, "rooms-chat");
+//tham số đầu là tên để gọi dữ liệu, tham số hai là khung, tham số ba là bảng dữ liệu
+module.exports = RoomChat;
